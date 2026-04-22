7b2a86d0241141ee957db92e1e2f0e383549cdc2
Swanand Buchake <swanandbuchake@gmail.com>
2026-04-22 20:22:13 +0530

    final fix

--- app/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{padding: "40px", fontFamily: "sans-serif"}}>
-      <h1>Golf App</h1>
-      <a href="/login">Go to Login</a>
-    </div>
-  );
-}
